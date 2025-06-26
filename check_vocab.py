c4109a8773c15ee663e91ecb3d3d60ab563809e3
student somya <jsomya629@gmail.com>
2025-06-26 12:05:41 +0530

    removing extra files

--- check_vocab.py
+++
@@ -1,6 +0,0 @@
-# check_vocab.py
-import pickle
-
-with open(r"E:\ImageProject\datasets\processed\Flickr8k\vocab.pkl", 'rb') as f:
-    vocab = pickle.load(f)
-print(f"Vocabulary size: {len(vocab)}")
